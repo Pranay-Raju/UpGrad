2f620e8250e4ddfc54425d2315e862be1e04695a
Pranay-Raju <Pranay-Raju@users.noreply.github.com>
2019-03-14 00:21:47 +0530

    Can Compare Students according to Constraints

--- upgrad.java
+++ upgrad.java
@@ -5,6 +5,11 @@ class Main{
 		Scanner sc = new Scanner(in);
 		int entries = sc.nextInt();
 		List<Student> list = new ArrayList<Student>();
+		Comparator<Student> comparator = new Comparator<Student>(){
+			public int compare(Student student1,Student student2){
+				return student2.compareTo(student1);
+			}
+		};
 		for(int i=1;i<=entries;i++){
 			String query = sc.next();
 			if(query.equals("SERVED")){
@@ -15,24 +20,41 @@ class Main{
 				float cgpa = sc.nextFloat();
 				int token = sc.nextInt();
 				list.add(new Student(name,cgpa,token));
+				list.sort(comparator);
 			}
-		}
-		for(Student student : list){
-			out.println(student);
 		}
 	}
 }
-class Student{
+class Student implements Comparable<Student>{
 	private String name;
-	private float cgpa;
-	private int token;
+	private Float cgpa;
+	private Integer token;
 
 	public Student(String name, float cgpa, int token){
 		this.name = name;
-		this.cgpa = cgpa;
-		this.token = token;
+		this.cgpa = new Float(cgpa);
+		this.token = new Integer(token);
 	}
 	public String toString(){
 		return "Student [ name = " + this.name + ", cgpa = " + this.cgpa + ", token = " + this.token + " ]";
+	}
+	public String getName(){
+		return this.name;
+	}
+	public Integer getToken(){
+		return this.token;
+	}
+	public Float getCGPA(){
+		return this.cgpa;
+	}
+	public int compareTo(Student student){
+		int compareResult = (this.cgpa).compareTo(student.getCGPA());
+		if(compareResult == 0){
+			compareResult = (this.name).compareTo(student.getName());
+		}
+		if(compareResult == 0){
+			compareResult = (this.token).compareTo(student.getToken());
+		}
+		return compareResult;
 	}
 }
